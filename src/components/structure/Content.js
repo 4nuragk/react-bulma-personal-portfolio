5487558184d06d842eded1ef90301462bff6c275
anurag_kushwaha <anurag.kushwaha365@gmail.com>
2020-06-05 03:29:10 +0530

    Added meta tags

--- src/components/structure/Content.js
+++ src/components/structure/Content.js
@@ -2,7 +2,6 @@ import React from "react";
 import AboutMe from "../sections/AboutMe";
 import Skills from "../sections/Skills";
 import Experience from "../sections/Experience";
-import Articles from "../sections/Articles";
 
 function Content() {
   return (
